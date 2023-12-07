178803cb5f837f6cce863db276979e0c482fc8ad
DAN <mozhayen@bk.ru>
2023-12-07 23:49:34 +0300

    changed the design and added a copy button

---
+++ logic/copu.js
@@ -0,0 +1,10 @@
+
+    function Copy(){
+        var copyText = document.getElementById("text");
+        var textArea = document.createElement("textarea");
+        textArea.value = copyText.textContent;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("Copy");
+        textArea.remove();
+    }
